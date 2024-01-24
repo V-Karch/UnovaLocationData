d19dac4d508cdcd9e4a51ff3d0d278ac1661604c
V-Karch <luna.karch05@gmail.com>
2024-01-24 10:32:04 -0500

    Started basic project skeleton with a few template classes

---
+++ src/main/java/program/EncounterTable.java
@@ -0,0 +1,36 @@
+package program;
+
+import java.util.ArrayList;
+
+public class EncounterTable {
+    private int minimumLevel;
+    private int maximumLevel;
+
+    private ArrayList<String> times;
+    private ArrayList<String> seasons;
+    private ArrayList<String> gameVersions;
+
+    private String rarity;
+    private String spriteUrl;
+    private String pokemonName;
+    private String formAnnotation;
+
+    public EncounterTable(int minimumLevel, int maximumLevel,
+    ArrayList<String> gameVersions, ArrayList<String> seasons,
+    ArrayList<String> times, String pokemonName, String formAnnotation, 
+    String rarity, String spriteUrl
+    ) {
+        this.minimumLevel = minimumLevel;
+        this.maximumLevel = maximumLevel;
+
+        this.times = times;
+        this.seasons = seasons;
+        this.gameVersions = gameVersions;
+        
+        this.rarity = rarity;
+        this.spriteUrl = spriteUrl;
+        this.pokemonName = pokemonName;
+        this.formAnnotation = formAnnotation;
+    }
+
+}

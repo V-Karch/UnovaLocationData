0716ab2582ebf9411c0e329f8ca2b8af18f80415
Luna-Karch <luna.karch05@gmail.com>
2024-02-09 02:41:37 -0500

    Added getFloor() and getLocationName()

--- src/main/java/program/DataTypes/Location.java
+++ src/main/java/program/DataTypes/Location.java
@@ -2,11 +2,17 @@ package program.DataTypes;
 
 public class Location {
     private Entry[] entries;
-    public Location(Entry[] entries) {
+    public String locationName;
+    public Location(Entry[] entries, String locationName) {
         this.entries = entries;
+        this.locationName = locationName;
     }
 
     public Entry[] getAllEntries() {
         return this.entries;
+    }
+
+    public String getLocationName() {
+        return this.locationName;
     }
 }

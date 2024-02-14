ce790a2c97acfcb2c9d709a74f14da2bfed1e389
Luna-Karch <luna.karch05@gmail.com>
2024-02-14 10:29:56 -0500

    Minor debugging fix, Location class now works as intended

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -1,7 +1,10 @@
 package program;
 
+import program.DataTypes.Location;
+
 public class Main {
     public static void main(String args[]) {
-        // Code
+        Location location = new Location("Route 1");
+        location.printAllEntries();
     } 
 }

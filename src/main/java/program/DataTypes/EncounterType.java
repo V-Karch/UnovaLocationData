b97a10982d179e7c4f0ae8f0434eafe11ecd9178
Luna-Karch <luna.karch05@gmail.com>
2024-02-14 09:39:51 -0500

    Implemented fromString method

--- src/main/java/program/DataTypes/EncounterType.java
+++ src/main/java/program/DataTypes/EncounterType.java
@@ -18,4 +18,23 @@ public enum EncounterType {
     public String toString() {
         return this.encounterTypeString;
     }
+
+    public static EncounterType fromString(String readString) {
+        switch (readString) {
+            case "Walking":
+                return EncounterType.WALKING;
+            case "Surfing":
+                return EncounterType.SURFING;
+            case "Super Rod":
+                return EncounterType.SUPER_ROD;
+            case "Interact":
+                return EncounterType.INTERACT;
+            case "Trade":
+                return EncounterType.TRADE;
+            case "Gift":
+                return EncounterType.GIFT;
+            default:
+                return EncounterType.WALKING;
+        }
+    }
 }

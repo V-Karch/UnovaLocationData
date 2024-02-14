45e2f4d1ba65923c31e9c01a1265d17ebd9c29b3
V-Karch <luna.karch05@gmail.com>
2024-02-14 10:02:59 -0500

    Added fromString for Season.java

--- src/main/java/program/DataTypes/Season.java
+++ src/main/java/program/DataTypes/Season.java
@@ -16,4 +16,50 @@ public enum Season {
     public String toString() {
         return this.seasonString;
     }
+    public static Season[] fromString(String readLine) {
+        if (readLine.contains("/")) { // Cannot be all
+            String[] seasonStrings = readLine.split("/");
+            Season[] result = new Season[seasonStrings.length];
+
+            for (int i = 0; i < seasonStrings.length; i++) {
+                switch (seasonStrings[i]) {
+                    case "Spring":
+                        result[i] = Season.SPRING;
+                        break;
+                    case "Summer":
+                        result[i] = Season.SUMMER;
+                        break;
+                    case "Autumn":
+                        result[i] = Season.AUTUMN;
+                        break;
+                    case "Winter":
+                        result[i] = Season.WINTER;
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        switch (readLine) {
+            case "All":
+                Season[] all = { Season.SPRING, Season.SUMMER, Season.AUTUMN, Season.WINTER };
+                return all;
+            case "Spring":
+                Season[] spring = { Season.SPRING };
+                return spring;
+            case "Summer":
+                Season[] summer = { Season.SUMMER };
+                return summer;
+            case "Autumn":
+                Season[] autumn = { Season.AUTUMN };
+                return autumn;
+            case "Winter":
+                Season[] winter = { Season.WINTER };
+                return winter;
+            default:
+                Season[] defaultSeasons = { Season.SPRING, Season.SUMMER, Season.AUTUMN, Season.WINTER };
+                return defaultSeasons;
+        }
+    }
 }

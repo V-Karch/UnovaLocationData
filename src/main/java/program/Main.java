bd6dd45aff4cd453ddb26a26150c12a3ab5112b6
V-Karch <luna.karch05@gmail.com>
2024-02-17 23:13:13 -0500

    Added a method to search a location by pokemon name, ensured it works

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -1,9 +1,16 @@
 package program;
 
+import program.DataTypes.Location;
+
+import java.util.ArrayList;
+
+import program.DataTypes.Entry;
 import program.DataTypes.LocationManager;
 
 public class Main {
     public static void main(String args[]) {
-        LocationManager.listValidLocations();
+        Location route1 = LocationManager.loadLocation("Route 1");
+        ArrayList<Entry> entries = route1.getEntriesByPokemonName("Basculin");
+        Entry.printGivenEntries(entries);
     } 
 }

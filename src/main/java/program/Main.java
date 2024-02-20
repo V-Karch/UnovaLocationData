2f63fd96675f149cd3f1df11302064027276b692
Luna-Karch <luna.karch05@gmail.com>
2024-02-20 16:13:03 -0500

    Added Castelia City/Castelia Sewers data

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -6,9 +6,8 @@ import program.DataTypes.Classes.CollectionManager;
 
 public class Main {
     public static void main(String args[]) {
-        Collection route14 = CollectionManager.loadCollection("Route 14");
+        Collection route14 = CollectionManager.loadCollection("Abundant Shrine");
         route14
-            .filter(FilterType.LEVEL, "58")
             .filter(FilterType.GAME_VERSION, "Black")
             .filter(FilterType.ENCOUNTER_TYPE, "Walking")
             .printAllEntries();

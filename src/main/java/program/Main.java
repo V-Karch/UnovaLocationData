aabfaa563fb03fb5a0e4c2960f3f971afd158541
Luna-Karch <luna.karch05@gmail.com>
2024-02-20 15:22:46 -0500

    Changed Main.java to use CollectionManager.java instead to load the collection

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -2,10 +2,11 @@ package program;
 
 import program.DataTypes.Enums.FilterType;
 import program.DataTypes.Classes.Collection;
+import program.DataTypes.Classes.CollectionManager;
 
 public class Main {
     public static void main(String args[]) {
-        Collection route14 = new Collection("Route 14");
+        Collection route14 = CollectionManager.loadCollection("Route 14");
         route14
             .filter(FilterType.LEVEL, "58")
             .filter(FilterType.GAME_VERSION, "Black")

b73d83c179159ec7423f1e31a2b4c92606e918f6
Luna-Karch <luna.karch05@gmail.com>
2024-03-21 15:56:46 -0400

    Added GUIMaker class

---
+++ src/main/java/program/GUI/GUIMaker.java
@@ -0,0 +1,11 @@
+package program.GUI;
+
+
+import javafx.scene.control.ComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import program.DataTypes.Enums.EncounterType;
+
+public class GUIMaker {
+    // Implement
+}

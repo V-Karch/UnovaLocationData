fa980f1b3ccd7995cd599e7a127747a4a23aa89d
V-Karch <luna.karch05@gmail.com>
2024-03-21 15:57:28 -0400

    Added encounterTypeDropdown() method which should return a dropdown box of all encounter methods

--- src/main/java/program/GUI/GUIMaker.java
+++ src/main/java/program/GUI/GUIMaker.java
@@ -7,5 +7,18 @@ import javafx.collections.ObservableList;
 import program.DataTypes.Enums.EncounterType;
 
 public class GUIMaker {
-    // Implement
+    public static ComboBox<EncounterType> encounterTypeDropdown() {
+        ObservableList<EncounterType> options = 
+            FXCollections.observableArrayList(
+                EncounterType.WALKING,
+                EncounterType.SURFING,
+                EncounterType.SUPER_ROD,
+                EncounterType.INTERACT,
+                EncounterType.TRADE,
+                EncounterType.GIFT
+            );
+
+        final ComboBox<EncounterType> dropdownMenu = new ComboBox<>(options);
+        return dropdownMenu;
+    }
 }

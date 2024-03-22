04dee86bf390d71e53f5b4b33965117245c35bea
Luna-Karch <luna.karch05@gmail.com>
2024-03-22 03:18:19 -0400

    Added gameVersionDropdown, may change in the future to allow for selecting all versions

--- src/main/java/program/GUI/GUIFactory.java
+++ src/main/java/program/GUI/GUIFactory.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ComboBox;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import program.DataTypes.Enums.GameVersion;
 import program.DataTypes.Enums.EncounterType;
 
 public class GUIFactory {
@@ -25,6 +26,24 @@ public class GUIFactory {
 
     public static Label encounterTypeLabel() {
         Label label = new Label("Encounter Type:");
+        return label;
+    }
+
+    public static ComboBox<GameVersion> gameVersionDropdown() {
+        ObservableList<GameVersion> options = 
+            FXCollections.observableArrayList(
+                GameVersion.BLACK,
+                GameVersion.WHITE,
+                GameVersion.BLACK2,
+                GameVersion.WHITE2
+            );
+
+        final ComboBox<GameVersion> dropdownMenu = new ComboBox<>(options);
+        return dropdownMenu;
+    }
+
+    public static Label gameVersionLabel() {
+        Label label = new Label("Game Version:");
         return label;
     }
 }
--- src/main/java/program/GUI/GUITesting.java
+++ src/main/java/program/GUI/GUITesting.java
@@ -13,8 +13,11 @@ public class GUITesting extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         GridPane gridPane = new GridPane();
-        gridPane.add(GUIFactory.encounterTypeLabel(), 0, 0);
-        gridPane.add(GUIFactory.encounterTypeDropdown(), 0, 1);
+        stage.setTitle("Unova Location Data Parser");
+        gridPane.add(GUIFactory.gameVersionLabel(), 0, 0);
+        gridPane.add(GUIFactory.gameVersionDropdown(), 0, 1);
+        gridPane.add(GUIFactory.encounterTypeLabel(), 1, 0);
+        gridPane.add(GUIFactory.encounterTypeDropdown(), 1, 1);
 
         Scene scene = new Scene(gridPane, 500, 500);
         stage.setScene(scene);

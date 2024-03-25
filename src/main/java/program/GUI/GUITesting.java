f96ac5f19c73e4498504a147751eccaaf52613dc
Luna-Karch <luna.karch05@gmail.com>
2024-03-25 10:25:54 -0400

    Added a field for Level

--- src/main/java/program/GUI/GUITesting.java
+++ src/main/java/program/GUI/GUITesting.java
@@ -17,18 +17,19 @@ public class GUITesting extends Application {
         gridPane.setVgap(5);
         gridPane.setStyle("-fx-background-color: #9695a0");
         stage.setTitle("Unova Location Data Parser");
+
         gridPane.add(GUIFactory.gameVersionLabel(), 0, 0);
         gridPane.add(GUIFactory.gameVersionDropdown(), 0, 1);
-        gridPane.add(GUIFactory.encounterTypeLabel(), 1, 0);
-        gridPane.add(GUIFactory.encounterTypeDropdown(), 1, 1);
-        gridPane.add(GUIFactory.modifierLabel(), 2, 0);
-        gridPane.add(GUIFactory.modifierDropdown(), 2, 1);
-        gridPane.add(GUIFactory.rarityLabel(), 0, 3);
-        gridPane.add(GUIFactory.rarityDropdown(), 0, 4);
-        gridPane.add(GUIFactory.seasonLabel(), 1, 3);
-        gridPane.add(GUIFactory.seasonDropdown(), 1, 4);
-        gridPane.add(GUIFactory.pokemonNameLabel(), 2, 3);
-        gridPane.add(GUIFactory.pokemonNameEntry(), 2, 4);
+        gridPane.add(GUIFactory.encounterTypeLabel(), 0, 2);
+        gridPane.add(GUIFactory.encounterTypeDropdown(), 0, 3);
+        gridPane.add(GUIFactory.modifierLabel(), 0, 4);
+        gridPane.add(GUIFactory.modifierDropdown(), 0, 5);
+        gridPane.add(GUIFactory.rarityLabel(), 0, 6);
+        gridPane.add(GUIFactory.rarityDropdown(), 0, 7);
+        gridPane.add(GUIFactory.seasonLabel(), 0, 8);
+        gridPane.add(GUIFactory.seasonDropdown(), 0, 9);
+        gridPane.add(GUIFactory.pokemonNameLabel(), 0, 10);
+        gridPane.add(GUIFactory.pokemonNameEntry(), 0, 11);
 
         Scene scene = new Scene(gridPane, 500, 500);
         stage.setScene(scene);

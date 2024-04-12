20714e34ed04b226bb1e39c3e99ee8cd8528f7d1
Luna-Karch <luna.karch05@gmail.com>
2024-04-12 07:20:06 -0400

    Major refactor, loads everything static, a singular time

--- src/main/java/program/GUI/RunGUI.java
+++ src/main/java/program/GUI/RunGUI.java
@@ -3,16 +3,12 @@ package program.GUI;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.application.Application;
 import program.DataTypes.Enums.Rarity;
 import program.DataTypes.Enums.Season;
-import program.DataTypes.Classes.Entry;
 import program.DataTypes.Enums.Modifier;
 import program.DataTypes.Enums.FilterType;
 import program.DataTypes.Enums.GameVersion;
@@ -37,72 +33,51 @@ public class RunGUI extends Application {
         gridPane.setPadding(new Insets(5, 5, 5, 5));
         stage.setTitle("Gen 5 Location Data Parser");
 
-        // Variable Setup
-        Label gameVersionLabel = GUIFactory.gameVersionLabel();
-        ComboBox<GameVersion> gameVersionDropdown = GUIFactory.gameVersionDropdown();
-        Label encounterTypeLabel = GUIFactory.encounterTypeLabel();
-        ComboBox<EncounterType> encounterTypeDropdown = GUIFactory.encounterTypeDropdown();
-        Label modifierLabel = GUIFactory.modifierLabel();
-        ComboBox<Modifier> modifierDropdown = GUIFactory.modifierDropdown();
-        Label rarityLabel = GUIFactory.rarityLabel();
-        ComboBox<Rarity> rarityDropdown = GUIFactory.rarityDropdown();
-        Label seasonLabel = GUIFactory.seasonLabel();
-        ComboBox<Season> seasonDropdown = GUIFactory.seasonDropdown();
-        Label pokemonNameLabel = GUIFactory.pokemonNameLabel();
-        TextField pokemonNameEntry = GUIFactory.pokemonNameEntry();
-        Label levelLabel = GUIFactory.levelLabel();
-        TextField levelEntry = GUIFactory.levelEntry();
-        Button resetButton = GUIFactory.resetButton();
-        Button searchButton = GUIFactory.searchButton();
-        Label locationLabel = GUIFactory.locationLabel();
-        ComboBox<String> locationDropdown = GUIFactory.locationDropDown();
-        TableView<Entry> entryTableView = GUIFactory.entryTableView();
-
         // Adding everything to the gridpane
-        gridPane.add(gameVersionLabel, 0, 0);
-        gridPane.add(gameVersionDropdown, 0, 1);
-        gridPane.add(encounterTypeLabel, 0, 2);
-        gridPane.add(encounterTypeDropdown, 0, 3);
-        gridPane.add(modifierLabel, 0, 4);
-        gridPane.add(modifierDropdown, 0, 5);
-        gridPane.add(rarityLabel, 0, 6);
-        gridPane.add(rarityDropdown, 0, 7);
-        gridPane.add(seasonLabel, 0, 8);
-        gridPane.add(seasonDropdown, 0, 9);
-        gridPane.add(locationLabel, 0, 10);
-        gridPane.add(locationDropdown, 0, 11);
-        gridPane.add(pokemonNameLabel, 0, 12);
-        gridPane.add(pokemonNameEntry, 0, 13);
-        gridPane.add(levelLabel, 0, 14);
-        gridPane.add(levelEntry, 0, 15);
-        gridPane.add(searchButton, 0, 16);
-        gridPane.add(resetButton, 0, 17);
-        gridPane.add(entryTableView, 1, 0, 2, 18);
+        gridPane.add(GUIFactory.gameVersionLabel, 0, 0);
+        gridPane.add(GUIFactory.gameVersionDropdown, 0, 1);
+        gridPane.add(GUIFactory.encounterTypeLabel, 0, 2);
+        gridPane.add(GUIFactory.encounterTypeDropdown, 0, 3);
+        gridPane.add(GUIFactory.modifierLabel, 0, 4);
+        gridPane.add(GUIFactory.modifierDropdown, 0, 5);
+        gridPane.add(GUIFactory.rarityLabel, 0, 6);
+        gridPane.add(GUIFactory.rarityDropdown, 0, 7);
+        gridPane.add(GUIFactory.seasonLabel, 0, 8);
+        gridPane.add(GUIFactory.seasonDropdown, 0, 9);
+        gridPane.add(GUIFactory.locationLabel, 0, 10);
+        gridPane.add(GUIFactory.locationDropDown, 0, 11);
+        gridPane.add(GUIFactory.pokemonNameLabel, 0, 12);
+        gridPane.add(GUIFactory.pokemonNameEntry, 0, 13);
+        gridPane.add(GUIFactory.levelLabel, 0, 14);
+        gridPane.add(GUIFactory.levelEntry, 0, 15);
+        gridPane.add(GUIFactory.searchButton, 0, 16);
+        gridPane.add(GUIFactory.resetButton, 0, 17);
+        gridPane.add(GUIFactory.entryTableView, 1, 0, 2, 18);
 
-        entryTableView.setStyle("-fx-background-color: #FFFFFF;");
-        entryTableView.setPrefWidth(931);
-        entryTableView.setMaxHeight(Double.MAX_VALUE);
-        entryTableView.setEditable(false);
+        GUIFactory.entryTableView.setStyle("-fx-background-color: #FFFFFF;");
+        GUIFactory.entryTableView.setPrefWidth(931);
+        GUIFactory.entryTableView.setMaxHeight(Double.MAX_VALUE);
+        GUIFactory.entryTableView.setEditable(false);
 
-        searchButton.setOnAction(event -> {
-            Collection results = search(gameVersionDropdown, 
-            encounterTypeDropdown, 
-            modifierDropdown, rarityDropdown, seasonDropdown, 
-            locationDropdown, pokemonNameEntry, levelEntry);
+        GUIFactory.searchButton.setOnAction(event -> {
+            Collection results = search(GUIFactory.gameVersionDropdown, 
+            GUIFactory.encounterTypeDropdown, 
+            GUIFactory.modifierDropdown, GUIFactory.rarityDropdown, GUIFactory.seasonDropdown, 
+            GUIFactory.locationDropDown, GUIFactory.pokemonNameEntry, GUIFactory.levelEntry);
 
-            GUIFactory.updateTable(entryTableView, results);
+            GUIFactory.updateTable(GUIFactory.entryTableView, results);
         });
 
-        resetButton.setOnAction(event -> {
-            gameVersionDropdown.getSelectionModel().clearSelection();
-            encounterTypeDropdown.getSelectionModel().clearSelection();
-            modifierDropdown.getSelectionModel().clearSelection();
-            rarityDropdown.getSelectionModel().clearSelection();
-            seasonDropdown.getSelectionModel().clearSelection();
-            locationDropdown.getSelectionModel().clearSelection();
-            pokemonNameEntry.clear();
-            levelEntry.clear();
-            entryTableView.getItems().clear();
+        GUIFactory.resetButton.setOnAction(event -> {
+            GUIFactory.gameVersionDropdown.getSelectionModel().clearSelection();
+            GUIFactory.encounterTypeDropdown.getSelectionModel().clearSelection();
+            GUIFactory.modifierDropdown.getSelectionModel().clearSelection();
+            GUIFactory.rarityDropdown.getSelectionModel().clearSelection();
+            GUIFactory.seasonDropdown.getSelectionModel().clearSelection();
+            GUIFactory.locationDropDown.getSelectionModel().clearSelection();
+            GUIFactory.pokemonNameEntry.clear();
+            GUIFactory.levelEntry.clear();
+            GUIFactory.entryTableView.getItems().clear();
         });
         
         Scene scene = new Scene(gridPane, 1140, 500);
@@ -121,7 +96,7 @@ public class RunGUI extends Application {
     TextField pokemonNameEntry, TextField levelEntry) {
 
         GameVersion gameVersion = gameVersionDropdown.getValue();
-        EncounterType encounterType = encounterTypeDropdown.getValue();
+        EncounterType encounterType = GUIFactory.encounterTypeDropdown.getValue();
         Modifier modifier = modifierDropdown.getValue();
         Rarity rarity = rarityDropdown.getValue();
         Season season = seasonDropdown.getValue();

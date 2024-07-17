52ca30ed9cbc98d4150702d754074f4d737fe430
V-Karch <luna.karch05@gmail.com>
2024-07-17 11:06:34 -0400

    Main.java: added TODO to add the rest of the missing locations

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -5,7 +5,8 @@ import program.GUI.RunGUI;
 public class Main {
     public static void main(String args[]) {
         /* TODO: Resize Window to account for the longest possible length of each field
-        * Locations: Driftveil Drawbridge, Length: 20
+        * Longest Location Name: Driftveil Drawbridge, Length: 20
+        * TODO: Add the rest of the missing locations
         */ 
         RunGUI.runGUI(args);
     }

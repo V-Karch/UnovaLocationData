1eb03173a05cced90af60fa0aaac6cb6f4ec68ed
V-Karch <luna.karch05@gmail.com>
2024-07-17 11:04:22 -0400

    Main.java: Added TODO so I remember to fix window size

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -4,6 +4,9 @@ import program.GUI.RunGUI;
 
 public class Main {
     public static void main(String args[]) {
+        /* TODO: Resize Window to account for the longest possible length of each field
+        * Locations: Driftveil Drawbridge, Length: 20
+        */ 
         RunGUI.runGUI(args);
     }
 }

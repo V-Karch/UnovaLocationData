a896c0a6731b45992d37855e06b5184b60bc084d
V-Karch <luna.karch05@gmail.com>
2024-10-07 16:58:38 -0400

    Main: Removed TODOs in favor of github issues

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -4,10 +4,6 @@ import program.GUI.RunGUI;
 
 public class Main {
     public static void main(String args[]) {
-        /* TODO: Resize Window to account for the longest possible length of each field
-        * Longest Location Name: Driftveil Drawbridge, Length: 20
-        * TODO: Add the rest of the missing locations
-        */ 
         RunGUI.runGUI(args);
     }
 }
